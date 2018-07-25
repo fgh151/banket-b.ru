3c8eae713d0c8262fef0d9f95e770515914ec45e
Fedor B Grosky <fedor@support-pc.org>
2018-07-25 15:33:07 +0300

    Test heroku

--- api/web/index.php
+++ api/web/index.php
@@ -1,4 +1,22 @@
 <?php
+//defined('YII_DEBUG') or define('YII_DEBUG', true);
+//defined('YII_ENV') or define('YII_ENV', 'dev');
+//
+//require __DIR__ . '/../../vendor/autoload.php';
+//require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
+//require __DIR__ . '/../../common/config/bootstrap.php';
+//require __DIR__ . '/../config/bootstrap.php';
+//
+//$config = yii\helpers\ArrayHelper::merge(
+//    require __DIR__ . '/../../common/config/main.php',
+//    require __DIR__ . '/../../common/config/main-local.php',
+//    require __DIR__ . '/../config/main.php',
+//    require __DIR__ . '/../config/main-local.php'
+//);
+//
+//(new yii\web\Application($config))->run();
+
+
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
@@ -9,9 +27,16 @@ require __DIR__ . '/../config/bootstrap.php';
 
 $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/../../common/config/main.php',
-    require __DIR__ . '/../../common/config/main-local.php',
+    require __DIR__ . '/../../common/config/heroku-local.php',
     require __DIR__ . '/../config/main.php',
-    require __DIR__ . '/../config/main-local.php'
+    [
+        'components' => [
+            'request' => [
+                // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+                'cookieValidationKey' => 'GK_DtXuAbQD31r07tNNZlocsZyogzC4a',
+            ],
+        ],
+    ]
 );
 
 (new yii\web\Application($config))->run();
