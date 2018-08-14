b18913ef9c65eadd42f3fc60de7854a5d558111e
Fedor B Gorsky <fedor@support-pc.org>
2018-08-14 11:12:36 +0300

    Restore password

--- api/controllers/AuthController.php
+++ api/controllers/AuthController.php
@@ -10,7 +10,9 @@ namespace app\api\controllers;
 
 use app\api\models\LoginForm;
 use app\api\models\RegisterForm;
+use app\common\models\MobileUser;
 use yii;
+use yii\base\Exception;
 use yii\helpers\Json;
 use yii\rest\Controller;
 
@@ -29,10 +31,11 @@ class AuthController extends Controller
         } else {
             $response = null;
             foreach ($model->errors as $error) {
-                $response = new \stdClass();
+                $response        = new \stdClass();
                 $response->error = $error[0];
             }
             Yii::$app->response->statusCode = 403;
+
             return $response;
         }
     }
@@ -46,7 +49,7 @@ class AuthController extends Controller
         $model = new RegisterForm();
 
         $request = Json::decode(Yii::$app->getRequest()->getRawBody(), true);
-        if ($model->load($request, '') ) {
+        if ($model->load($request, '')) {
             if ($key = $model->register()) {
                 return ['access_token' => $key];
             } else {
@@ -56,9 +59,129 @@ class AuthController extends Controller
                     $response['error'] = $error[0];
                 }
                 Yii::$app->response->statusCode = 403;
+
                 return $response;
             }
         }
+
         return false;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function actionRecover()
+    {
+        $request = Json::decode(Yii::$app->getRequest()->getRawBody(), true);
+        $phone   = $request['phone'];
+
+        /**
+         * Находим человека и отправляем еу СМС
+         */
+        $user = MobileUser::find()->where(['phone' => $phone])->one();
+        if ($user) {
+
+            $user->password_reset_token = Yii::$app->security->generateRandomKey(6);
+            $user->save();
+
+
+            $this->sendSms(
+                'Код подтверждения '+$user->password_reset_token,
+                $user->phone,
+                'ac2ea781ced8dd8a479849addcd758a6',
+                '0012c0439ade32a5c19974d4053b22f8'
+            );
+        }
+    }
+
+    /**
+     * @return array
+     * @throws Exception
+     */
+    public function actionChangePassword()
+    {
+
+        $request     = Json::decode(Yii::$app->getRequest()->getRawBody(), true);
+        $code        = $request['code'];
+        $newPassword = $request['newPassword'];
+
+        /** @var MobileUser $user */
+        $user = MobileUser::find()->where(['password_reset_token' => $code])->one();
+        if ($user) {
+
+            $user->password_reset_token = null;
+            $user->setPassword($newPassword);
+            $user->generateAuthKey();
+
+            return ['access_token' => $user->getAuthKey()];
+        }
+
+
+    }
+
+
+    /**
+     * @param $text
+     * @param $phone
+     * @param $key
+     * @param $secret
+     *
+     * @throws Exception
+     */
+    private function sendSms($text, $phone, $key, $secret)
+    {
+        $smsParams = [
+            'datetime'     => '',
+            'key'          => $key,
+            'sender'       => 'restorate',
+            'sms_lifetime' => 1,
+            'type'         => 2,
+
+        ];
+
+        $resultParams = yii\helpers\ArrayHelper::merge($smsParams, [
+            'phone' => $phone,
+            'text' => $text
+        ]);
+
+        $hash = $this->getSum($resultParams, $secret);
+        $resultParams['sum'] = $hash;
+
+        $Curl = curl_init();
+        $CurlOptions = [
+            CURLOPT_URL            => 'http://api.myatompark.com/sms/3.0/sendSMS',
+            CURLOPT_FOLLOWLOCATION => false,
+            CURLOPT_POST           => true,
+            CURLOPT_HEADER         => false,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CONNECTTIMEOUT => 15,
+            CURLOPT_TIMEOUT        => 100,
+            CURLOPT_POSTFIELDS     => $resultParams,
+        ];
+        curl_setopt_array($Curl, $CurlOptions);
+        if (false === ($Result = curl_exec($Curl))) {
+            throw new Exception('Http request failed');
+        }
+
+        curl_close($Curl);
+
+        echo $Result;
+    }
+
+    private function  getSum($options, $secret)
+    {
+        $sumParams = [
+            'action'  => 'sendSMS',
+            'version' => '3.0'
+        ];
+        $resultParams = yii\helpers\ArrayHelper::merge($options, $sumParams);
+        $sortedKeys = array_keys($resultParams);
+        $resultString = '';
+
+        foreach ($sortedKeys as $key) {
+            $resultString .=$resultParams[$key];
+        }
+        $resultString .= $secret;
+        return md5($resultString);
     }
 }
