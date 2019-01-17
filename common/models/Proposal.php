7b51e5ec513a47492a84801a12d15f56b502c2e6
Fedor B Gorsky <fedor@support-pc.org>
2019-01-17 12:11:35 +0300

    Почту получают не оплаченные

--- common/models/Proposal.php
+++ common/models/Proposal.php
@@ -285,7 +285,7 @@ class Proposal extends ActiveRecord
         }
         if ($insert) {
             $recipients = Organization::find()
-                ->where(['state' => Constants::ORGANIZATION_STATE_PAID])
+//                ->where(['state' => Constants::ORGANIZATION_STATE_PAID])
                 ->andFilterWhere(['NOT ILIKE', 'email', 'banket-b.ru'])
                 ->all();
             foreach ($recipients as $recipient) {
